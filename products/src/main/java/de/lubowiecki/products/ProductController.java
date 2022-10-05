e036f1bbf4da929c873ca3652b030b9a316cf823
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2022-10-05 10:53:42 +0200

    Update products, Spring

--- products/src/main/java/de/lubowiecki/products/ProductController.java
+++ products/src/main/java/de/lubowiecki/products/ProductController.java
@@ -1,13 +1,32 @@
 package de.lubowiecki.products;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
 	
-	@RequestMapping("/") // http://localhost:8080/xyz
-	public String machWas() {
-		return "basic"; // basic.html wird in resources/templates gesucht
+	private ProductRepository repository = new ProductRepository();
+		
+	@RequestMapping("/") // http://localhost:8080/
+	public String showAll(Model model) {
+		model.addAttribute("headline", "Alle Produkte");
+		model.addAttribute("products", repository.findAll()); // Sendet alle Produkte an das Template
+		return "list"; // list.html wird in resources/templates gesucht
+	}
+	
+	@GetMapping("/add") // http://localhost:8080/add
+	public String showForm(Model model) {
+		model.addAttribute("headline", "Neues Produkt");
+		return "form";
+	}
+	
+	@PostMapping("/add")
+	public String processForm(Product product) {
+		repository.add(product);
+		return "redirect:/"; // umleitung auf http://localhost:8080/
 	}
 }
---
+++ products/src/main/java/de/lubowiecki/products/ProductRepository.java
@@ -0,0 +1,37 @@
+package de.lubowiecki.products;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// CRUD: Create, Read, Update, Delete
+public class ProductRepository {
+	
+	private int nextId;
+	
+	private ArrayList<Product> products = new ArrayList<>();
+	
+	public void add(Product product) {
+		product.setId(++nextId);
+		products.add(product);
+	}
+	
+	public Product findById(int id) {
+		throw new UnsupportedOperationException("Noch nicht implementiert");
+	}
+	
+	public List<Product> findAll() {
+		return products;
+	}
+	
+	public void update(Product product, int id) {
+		throw new UnsupportedOperationException("Noch nicht implementiert");
+	}
+	
+	public void delete(Product product) {
+		throw new UnsupportedOperationException("Noch nicht implementiert");
+	}
+	
+	public void deleteById(int id) {
+		throw new UnsupportedOperationException("Noch nicht implementiert");
+	}
+}
